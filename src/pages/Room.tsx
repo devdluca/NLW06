1a0674a7c57b12f492348832c69400db479d9cc0
devdluca <felipedelucamendes@gmail.com>
2021-06-24 17:14:03 -0300

    codigo da sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,16 +1,44 @@
+import { useState } from 'react';
+
+import { useParams } from 'react-router-dom'
+
 import logoImg from "../assets/images/logo.svg";
 
 import { Button } from "../components/Button";
 
-import "../styles/room.scss";   
+import { RoomCode } from '../components/RoomCode';
+
+
+
+import "../styles/room.scss";
+
+type RoomParams = {
+  id: string;
+}
 
 export function Room() {
+  const user = useAuth();
+  const params = useParams<RoomParams>();
+  const [newQuestion, setNewQuestion] = useState ('');
+  const roomId = params.id;
+
+  async function handleSendQuestions(){
+
+      if (newQuestion.trim( ) === ''){
+        return;
+      }
+
+      if (!user){
+        throw new error('Você não fez o login');
+      }
+  }
+
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <div>codigo</div>
+          <RoomCode code={params.id} />
         </div>
       </header>
 
@@ -22,6 +50,8 @@ export function Room() {
 
         <form>
           <textarea placeholder="O que você quer perguntar?"
+            onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
           />
 
           <div className="form-footer">
